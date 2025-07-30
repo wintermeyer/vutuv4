045b43069137e0f09012de6aa2c1ba655c87ebda
Stefan Wintermeyer <sw@wintermeyer-consulting.de>
2025-07-30 09:45:22 +0200

    Add igniter

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Vutuv.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:igniter, "~> 0.6", only: [:dev, :test]}
     ]
   end
 
